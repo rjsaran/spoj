74767dc80c7ed5153afd1fe426d867b961265f24
ramjeet saran <genuse.ramjeet@gmail.com>
2015-08-12 12:06:31 +0530

    added arrrev

--- addrev.cpp
+++ addrev.cpp
@@ -12,19 +12,40 @@ using namespace std;
 
 int main()
 {
-	int t;
-	double A,B;
+	long int t;
 	cin>>t;
 	while(t--){
+		string A, B;
+		long long int lenA, lenB, out = 0;
 		cin>>A>>B;
-		int steps = 0;
-		while(log2(A) != (int)log2(A)){	
-			A = ((int)A)/2;
-			steps++;
+		lenA = A.length();
+		lenB = B.length();
+		long long int i = 0, j = 0;
+		long long int c = 0, mool;
+		while(i < lenA && j < lenB){
+			mool = c + (A[i] - '0') + (B[i] - '0');
+			out = out * 10 + mool % 10;
+			c = mool > 9 ? 1 : 0 ; 
+			i++;
+			j++;
+			//cout<<out<<" "<<i+1<<endl;
 		}
-		if(A<B) steps += log2(B) - log2(A);
-		else steps += log2(A) - log2(B);
-
-		cout<<steps<<endl;
+		while(i < lenA)
+			{
+				mool = c + (A[i] - '0');
+				out = out * 10 + mool % 10;
+				c = mool > 9 ? 1 : 0 ;
+				i++;
+			}
+		while(j < lenB)
+			{
+				mool = c + (B[j] - '0');
+				out = out * 10 + mool % 10;
+				c = mool > 9 ? 1 : 0 ;
+				j++;
+			}
+		if(c == 1)
+			out = out * 10 + c ;
+		cout<<out<<endl;
 	}
 }
